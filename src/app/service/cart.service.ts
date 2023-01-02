b2c2667a0b504ab761be715d3cb3dd820df54a26
Prakashvyawhare <121247686+Prakashvyawhare@users.noreply.github.com>
2023-01-02 15:10:38 +0530

    max quantity validation and no repeat item in cart

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -1,6 +1,8 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { count } from 'console';
 import { cartItem } from './cartItem';
+import { UserDetailsService } from './user-details.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +12,33 @@ export class  CartService {
 MyCart=new Array<cartItem>();
 // currentUserCart= Array<cartItem>();
 
-constructor() { }
+constructor(public getusername : UserDetailsService) { }
 getCartItembyUserName(username:string){
   let currentUserCart=this.MyCart.filter((x)=>{
     return x.userName==username})
   return currentUserCart;
 }
 
-  addProducttoCart(product: cartItem ) {
-   this.MyCart.push(product);
+addProducttoCart(product: cartItem ) {  ////  if this product not exist in cart then add to cart else add quantity of same product  ///
+   
+    let presentItem:any = this.getCartItembyUserName(this.getusername.ThisUser[0]).find(item=>item.productID==product.productID)   ///this item is already exist  ////
+    if(!presentItem)           
+    {
+    
+   this.MyCart.push(product)
+   alert("successfully added to the Cart")}
+   
+   else
+   alert("successfully added Quantity of this item")
+   presentItem.Qnty+=product.Qnty
    
   }
+
+
   removeProductFromCart(indexof:number){
     this.MyCart.splice(indexof,1);
   }
-  addQuantity(indexof:number){
+  addQuantity(indexof:number){      
     if(this.MyCart[indexof].Qnty>=5)
     alert("You can buy only upto 5 units of this product");
     else
