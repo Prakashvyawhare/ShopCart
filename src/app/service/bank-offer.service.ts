9a0f004c23c45e0ea5addb7502c07deb8fce7413
Prakashvyawhare <121247686+Prakashvyawhare@users.noreply.github.com>
2023-01-26 17:16:14 +0530

    Retrieve Offers from database

--- src/app/service/bank-offer.service.ts
+++ src/app/service/bank-offer.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Bank, } from '../cart/bankOffer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BankOfferService {
-offers= Array<Bank>()
-  constructor() { 
-    let COD = new Bank(0,"cash on Delivery",0,"no offer on cash on delivery ");
-    let icicibank = new Bank(1,"ICICI",100,"Bank Offer $100 off on ICICI bank on orders of $1000 and above");  
-    let hdfcbank  = new Bank(2,'HDFC',110,"Bank Offer $110 off on HDFC bank on orders of $1000 and above");
-    let Sbi = new Bank(3,"SBI",120,"Bank Offer $120 off on SBI bank on orders of $1000 and above");
-    this.offers.push(COD,icicibank,hdfcbank,Sbi);  
+offers= Array<any>()
+  constructor(private AngularFirestore: AngularFirestore) { 
+    this.getOffersfromDatabase()  
+    // let COD = new Bank(0,"cash on Delivery",0,"no offer on cash on delivery ");
+    // let icicibank = new Bank(1,"ICICI",100,"Bank Offer $100 off on ICICI bank on orders of $1000 and above");  
+    // let hdfcbank  = new Bank(2,'HDFC',110,"Bank Offer $110 off on HDFC bank on orders of $1000 and above");
+    // let Sbi = new Bank(3,"SBI",120,"Bank Offer $120 off on SBI bank on orders of $1000 and above");
+    // this.offers.push(COD,icicibank,hdfcbank,Sbi);  
+  }
+  getOffersfromDatabase(){   ////  Retrieve Offers from database  ////
+    this.AngularFirestore.collection('offers').valueChanges().subscribe((data)=>{ this.offers=data})
   }
 }
